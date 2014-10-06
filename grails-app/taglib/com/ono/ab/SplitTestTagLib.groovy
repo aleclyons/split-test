42b7b8e68eaf9394a772d3419cd550cdf5d8996d
Alec Lyons <alec.lyons@bcsg.com>
2014-10-06 12:42:22 +0100

    Removing attempt to render split test content

--- grails-app/taglib/com/ono/ab/SplitTestTagLib.groovy
+++ grails-app/taglib/com/ono/ab/SplitTestTagLib.groovy
@@ -3,7 +3,6 @@ package com.ono.ab
 class SplitTestTagLib {
 
     def splitTestService
-    def groovyPagesTemplateEngine
 
     static namespace = "ab"
 
@@ -15,10 +14,7 @@ class SplitTestTagLib {
                 SplitTestVariantHit splitTestVariantHit = splitTestService.loadSplitTestVariantHit(splitTestName)
                 if (splitTestVariantHit != null) {
                     println "Rendering contents of split test variant for ${splitTest?.name}."
-                    def stringWriter = new StringWriter()
-                    //TODO add to cache
-                    groovyPagesTemplateEngine.createTemplate(splitTestVariantHit.splitTestVariant.code,splitTestName).make(attrs.model).write(stringWriter)
-                    out << stringWriter.toString()
+                    out << splitTestVariantHit.splitTestVariant.code
                 } else {
                     println "Split test hit is null. Rendering default content."
                     out << body()
@@ -35,3 +31,4 @@ class SplitTestTagLib {
     }
 
 }
+
